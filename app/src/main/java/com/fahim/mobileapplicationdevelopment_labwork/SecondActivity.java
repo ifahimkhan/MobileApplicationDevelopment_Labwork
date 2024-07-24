15bd9c3e4a99522a981be8b91eef11f22a7b4e89
ifahimkhan <khan.fahim95@hotmail.com>
2024-07-24 19:59:39 +0530

    If applied this commit will add activity lifecycle.

--- app/src/main/java/com/fahim/mobileapplicationdevelopment_labwork/SecondActivity.java
+++ app/src/main/java/com/fahim/mobileapplicationdevelopment_labwork/SecondActivity.java
@@ -1,58 +1,60 @@
 package com.fahim.mobileapplicationdevelopment_labwork;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
-    private String TAG = MainActivity.class.getName();
+public class SecondActivity extends AppCompatActivity {
+
+    private static final String TAG = SecondActivity.class.getName();
+
+    public static void getInstance(Context context) {
+        context.startActivity(new Intent(context, SecondActivity.class));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Log.e(TAG, "onCreate: ");
+        setContentView(R.layout.activity_second);
+        Log.w(TAG, "onCreate: ");
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Log.e(TAG, "onStart: ");
+        Log.w(TAG, "onStart: ");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.e(TAG, "onResume: ");
+        Log.w(TAG, "onResume: ");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Log.e(TAG, "onPause: ");
+        Log.w(TAG, "onPause: ");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Log.e(TAG, "onStop: ");
+        Log.w(TAG, "onStop: ");
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
-        Log.e(TAG, "onRestart: ");
+        Log.w(TAG, "onRestart: ");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.e(TAG, "onDestroy: ");
-    }
-
-    public void gotoSecondActivity(View view) {
-        SecondActivity.getInstance(MainActivity.this);
+        Log.w(TAG, "onDestroy: ");
     }
 }
