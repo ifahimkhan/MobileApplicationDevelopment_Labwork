5209ee2faa121bca59af46b07c65aafee484ca3e
ifahimkhan <khan.fahim95@hotmail.com>
2024-07-31 21:07:19 +0530

    Added Implicit Intents

--- app/src/main/java/com/fahim/mobileapplicationdevelopment_labwork/SecondActivity.java
+++ app/src/main/java/com/fahim/mobileapplicationdevelopment_labwork/SecondActivity.java
@@ -3,6 +3,8 @@ package com.fahim.mobileapplicationdevelopment_labwork;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.provider.MediaStore;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,7 +21,7 @@ public class SecondActivity extends AppCompatActivity {
     public void launchMap(View view) {
 
         Intent intent = new Intent(android.content.Intent.ACTION_VIEW);
-        intent.setData(Uri.parse("geo:19.0760, 72.8777"));
+        intent.setData(Uri.parse("geo:19.0760, 72.8777?q=Mukesh Patel School of Technology Management Vile Parle"));
         Intent chooserIntent = Intent.createChooser(intent, "Launch Map");
         startActivity(chooserIntent);
     }
@@ -41,5 +43,34 @@ public class SecondActivity extends AppCompatActivity {
         intent.setType("message/rfc822");
         Intent chooserIntent = Intent.createChooser(intent, "Send Email");
         startActivity(chooserIntent);
+    }
+
+    public void makeCall(View view) {
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:123456789"));
+        Intent chooserIntent = Intent.createChooser(intent, "Choose Phone App");
+        startActivity(chooserIntent);
+    }
+
+    public void takePicture(View view) {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        Intent chooserIntent = Intent.createChooser(intent, "Choose Phone App");
+        startActivityForResult(chooserIntent, 101);
+    }
+
+    public void sendSms(View view) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, "Hello, world!");
+        startActivity(Intent.createChooser(intent, "Share via"));
+    }
+
+    public void openContact(View view) {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType(ContactsContract.Contacts.CONTENT_TYPE);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+
     }
 }
