86cf9c25c442652cb2132aa8bdaeacea6963b47f
ifahimkhan <khan.fahim95@hotmail.com>
2024-08-09 08:37:14 +0530

    If applied this commit will add Menu and Date Picker

--- app/src/main/java/com/fahim/mobileapplicationdevelopment_labwork/MainActivity.java
+++ app/src/main/java/com/fahim/mobileapplicationdevelopment_labwork/MainActivity.java
@@ -1,28 +1,79 @@
 package com.fahim.mobileapplicationdevelopment_labwork;
 
-import android.app.Activity;
-import android.content.Context;
+import android.annotation.SuppressLint;
+import android.app.DatePickerDialog;
 import android.os.Bundle;
-import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Spinner;
+import android.widget.DatePicker;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.ArrayList;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
 
+    private TextView dateSelectedTextView;
+    private DatePicker datePicker;
+    @SuppressLint("NewApi")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.button);
+        dateSelectedTextView = findViewById(R.id.textView);
+        datePicker = findViewById(R.id.datePickerview);
+        datePicker.setOnDateChangedListener(new DatePicker.OnDateChangedListener() {
+            @Override
+            public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                dateSelectedTextView.setText("Selected Date is: "+dayOfMonth+"-"+(monthOfYear
+                )+"-"+year);
+
+            }
+        });
     }
+
+    public void showDatePickerDialog(View view) {
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
+        DatePickerDialog dialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                dateSelectedTextView.setText("Selected Date is: "+dayOfMonth+"-"+(month+1)+"-"+year);
+            }
+        }, year, month, dayOfMonth);
+        dialog.show();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.about){
+            Toast.makeText(this, "About is selected", Toast.LENGTH_SHORT).show();
+        }
+        if (item.getItemId() == R.id.camera){
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+
 }
