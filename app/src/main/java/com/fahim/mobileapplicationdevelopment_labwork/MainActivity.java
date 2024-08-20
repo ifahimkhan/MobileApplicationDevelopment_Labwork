9a5408d431c44b6e3cb501cdcd5675f371db4ee3
ifahimkhan <khan.fahim95@hotmail.com>
2024-08-20 18:58:12 +0530

    if applied this commit will implement Displaying Images in imageview in android.

--- app/src/main/java/com/fahim/mobileapplicationdevelopment_labwork/MainActivity.java
+++ app/src/main/java/com/fahim/mobileapplicationdevelopment_labwork/MainActivity.java
@@ -2,27 +2,73 @@ package com.fahim.mobileapplicationdevelopment_labwork;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageView = findViewById(R.id.imageView);
+
+    }
+
+    public void selectImage(View view) {
+        Intent photoPickerIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        photoPickerIntent.setType("image/*");
+        startActivityForResult(photoPickerIntent, 1);
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == 1)
+        {
+            Uri chosenImageUri = data.getData();
+
+            Bitmap mBitmap = null;
+            try {
+                mBitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), chosenImageUri);
+                imageView.setImageBitmap(mBitmap);
+            } catch (IOException e) {
+                Log.e("TAG", "error loading image", e);
+                Toast.makeText(this, "error loading image", Toast.LENGTH_SHORT).show();
+            }
+
+        }
+        else if (resultCode == RESULT_OK && requestCode == 2) {
+            Bundle extras = data.getExtras();
+            Bitmap mBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(mBitmap);
+        }
+    }
+
+    public void captureImage(View view) {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent,2);
     }
 }
