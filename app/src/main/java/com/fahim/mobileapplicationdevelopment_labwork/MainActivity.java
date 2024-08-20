98744fb0e2b81589f102df01da211dfe188cf085
ifahimkhan <khan.fahim95@hotmail.com>
2024-08-20 17:44:45 +0530

    practice session

--- app/src/main/java/com/fahim/mobileapplicationdevelopment_labwork/MainActivity.java
+++ app/src/main/java/com/fahim/mobileapplicationdevelopment_labwork/MainActivity.java
@@ -1,28 +1,54 @@
 package com.fahim.mobileapplicationdevelopment_labwork;
 
-import android.app.Activity;
-import android.content.Context;
+import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.View;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Spinner;
+import android.widget.DatePicker;
 import android.widget.TextView;
-import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.ArrayList;
-
 public class MainActivity extends AppCompatActivity {
-
+    TextView textView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.date);
+    }
+
+    public void showDialog(View view) {
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this);
+        datePickerDialog.show();
+        datePickerDialog.setOnDateSetListener(new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                textView.setText("Date"+dayOfMonth+"/"+(month+1)+"/"+year);
+            }
+        });
+
+
+    }
+
+    public void showNextScreen(View view) {
+        Intent intent = new Intent(this, SecondActivity.class);
+        intent.putExtra("DateSelected", textView.getText().toString());
+        startActivity(intent);
+    }
+
+    public void launchCamera(View view) {
+        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+        startActivityForResult(intent,100);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==100){
+            textView.setText("Image Captured");
+        }
     }
 }
