688b3f43b14003eef1365031f314c0cac17064b6
ifahimkhan <khan.fahim95@hotmail.com>
2024-09-13 08:20:00 +0530

    implementing Executor Service in Android

--- app/src/main/java/com/fahim/mobileapplicationdevelopment_labwork/MainActivity.java
+++ app/src/main/java/com/fahim/mobileapplicationdevelopment_labwork/MainActivity.java
@@ -17,6 +17,8 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
@@ -24,6 +26,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -87,10 +90,86 @@ public class MainActivity extends AppCompatActivity {
 
         Future<Integer> future = executor.submit(calculateSquare);
         try {
-            int result = future.get(); // Waits for the result
+            int result = future.get();
             System.out.println("Square is: " + result);
         } catch (InterruptedException | ExecutionException e) {
             e.printStackTrace();
+        }
+
+        executor.shutdown();
+
+    }
+
+    public void handleLongRunningTask(View view) {
+        ExecutorService executor = Executors.newFixedThreadPool(4);
+
+        List<String> files = Arrays.asList("file1.txt", "file2.txt", "file3.txt", "file4.txt");
+
+        for (String file : files) {
+            executor.submit(() -> {
+                System.out.println("Processing " + file + " by: " + Thread.currentThread().getName());
+            });
+        }
+
+        executor.shutdown();
+
+    }
+
+    public void timeoutTask(View view) {
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+
+        Callable<String> longRunningTask = () -> {
+            Log.e("TAG", "starting long running Task: ");
+            Thread.sleep(6000);
+            return "Task completed";
+        };
+
+        Future<String> future = executor.submit(longRunningTask);
+        try {
+            Log.e("TAG", "starting wait time: ");
+            String result = future.get();
+            System.out.println(result);
+        } catch (Exception e) {
+            System.out.println("Task took too long, canceling...");
+            future.cancel(true);
+        }
+        Log.e("TAG", "ending wait time: ");
+
+        executor.shutdown();
+
+    }
+
+    public void databaseOperation(View view) {
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+
+        Runnable readFromDatabaseTask = () -> {
+            try {
+                Thread.sleep(2000);
+                System.out.println("Data read from the database by: " + Thread.currentThread().getName());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        };
+
+        executor.submit(readFromDatabaseTask);
+        executor.shutdown();
+
+    }
+
+    public void parallelProcessing(View view) {
+        ExecutorService executor = Executors.newFixedThreadPool(3);
+
+        List<String> images = Arrays.asList("image1.jpg", "image2.jpg", "image3.jpg");
+
+        for (String image : images) {
+            executor.submit(() -> {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                   e.printStackTrace();
+                }
+                System.out.println("Processing " + image + " by: " + Thread.currentThread().getName());
+            });
         }
 
         executor.shutdown();
