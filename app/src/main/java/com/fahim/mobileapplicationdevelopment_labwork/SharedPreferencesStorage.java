a4375edfe6cb0fdc1a6de482fce32ba7b58a6b34
ifahimkhan <khan.fahim95@hotmail.com>
2024-09-23 23:28:57 +0530

    encrypted shared preferences

--- app/src/main/java/com/fahim/mobileapplicationdevelopment_labwork/SharedPreferencesStorage.java
+++ app/src/main/java/com/fahim/mobileapplicationdevelopment_labwork/SharedPreferencesStorage.java
@@ -3,6 +3,12 @@ package com.fahim.mobileapplicationdevelopment_labwork;
 import android.content.Context;
 import android.content.SharedPreferences;
 
+import androidx.security.crypto.EncryptedSharedPreferences;
+import androidx.security.crypto.MasterKeys;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
 public class SharedPreferencesStorage {
 
     private static final String SHARED_PREFS = "userPrefs";
@@ -12,9 +18,35 @@ public class SharedPreferencesStorage {
     private SharedPreferences sharedPreferences;
     private SharedPreferences.Editor editor;
 
+//    private SharedPreferences encryptedSharedPreferences;
+//    private SharedPreferences.Editor encryptedEditor;
+
     public SharedPreferencesStorage(Context context) {
         sharedPreferences = context.getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE);
         editor = sharedPreferences.edit();
+        /*String masterKeyAlias = null;
+        try {
+            masterKeyAlias = MasterKeys.getOrCreate(MasterKeys.AES256_GCM_SPEC);
+        } catch (GeneralSecurityException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            encryptedSharedPreferences = EncryptedSharedPreferences.create(
+                    "secret_shared_prefs",
+                    masterKeyAlias,
+                    context,
+                    EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+                    EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+            );
+
+            encryptedEditor = encryptedSharedPreferences.edit();
+        } catch (GeneralSecurityException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }*/
     }
 
     public void saveUserData(String data) {
