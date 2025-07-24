cf2891814fe43e44942436fa400fb92d9ed65000
ifahimkhan <khan.fahim95@hotmail.com>
2025-07-24 09:52:36 +0530

    committing basic calculator

--- app/src/main/java/com/fahim/mobileapplicationdevelopment_labwork/MainActivity.java
+++ app/src/main/java/com/fahim/mobileapplicationdevelopment_labwork/MainActivity.java
@@ -1,11 +1,8 @@
 package com.fahim.mobileapplicationdevelopment_labwork;
 
-import android.app.Activity;
-import android.content.Context;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,82 +10,87 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText number1, number2;
-    private Button equal;
-    private TextView result;
-    private Spinner operators;
+    EditText etNumber1, etNumber2;
+    Spinner spinnerOperators;
+    Button equalBtn;
+    TextView resultTextview;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        number1 = findViewById(R.id.number1);
-        number2 = findViewById(R.id.number2);
-        equal = findViewById(R.id.equal);
-        result = findViewById(R.id.result);
-        operators = findViewById(R.id.operators);
-        ArrayList<String> operatorList = new ArrayList<>();
-        operatorList.add("+");
-        operatorList.add("-");
-        operatorList.add("*");
-        operatorList.add("/");
-        ArrayAdapter<String> adaper = new ArrayAdapter<>(this, R.layout.row_dropdown,R.id.item, operatorList);
-        operators.setAdapter(adaper);
-        equal.setOnClickListener(new View.OnClickListener() {
+
+        etNumber1 = findViewById(R.id.etNumber1);
+        etNumber2 = findViewById(R.id.etNumber2);
+        spinnerOperators = findViewById(R.id.spinnerOperators);
+        equalBtn = findViewById(R.id.equalBtn);
+        resultTextview = findViewById(R.id.resultTextview);
+
+        ArrayList<String> operators = new ArrayList<>();
+        operators.add("+");
+        operators.add("-");
+        operators.add("*");
+        operators.add("/");
+
+        ArrayAdapter<String> adapter =
+                new ArrayAdapter<>(this, R.layout.row_dropdown, R.id.item, operators);
+        spinnerOperators.setAdapter(adapter);
+
+        equalBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                hideKeyboard(MainActivity.this);
-                String num1 = number1.getText().toString();
-                String num2 = number2.getText().toString();
-                String operator = operators.getSelectedItem().toString();
-                if (TextUtils.isEmpty(num1)){
-                    number1.setError("Field cannot be empty");
-                    Toast.makeText(MainActivity.this, "Fields cannot be empty", Toast.LENGTH_SHORT).show();
+                String number1 = etNumber1.getText().toString();
+                String number2 = etNumber2.getText().toString();
+                String operator = spinnerOperators.getSelectedItem().toString();
+
+                if (TextUtils.isEmpty(number1)) {
+                    etNumber1.setError("Please enter a number");
                     return;
                 }
-                if (TextUtils.isEmpty(num2)){
-                    number2.setError("Field cannot be empty");
-                    Toast.makeText(MainActivity.this, "Fields cannot be empty", Toast.LENGTH_SHORT).show();
+                if (TextUtils.isEmpty(number2)) {
+                    etNumber2.setError("Please enter a number");
                     return;
                 }
-                int num1Int = Integer.parseInt(num1);
-                int num2Int = Integer.parseInt(num2);
-                int resultInt = 0;
+
+                double num1 = Double.parseDouble(number1);
+                double num2 = Double.parseDouble(number2);
+                double result = 0;
+
                 switch (operator) {
                     case "+":
-                        resultInt = num1Int + num2Int;
+                        result = num1 + num2;
                         break;
                     case "-":
-                        resultInt = num1Int - num2Int;
+                        result = num1 - num2;
                         break;
                     case "*":
-                        resultInt = num1Int * num2Int;
+                        result = num1 * num2;
                         break;
                     case "/":
-                        resultInt = num1Int / num2Int;
+                        if (num2 != 0) {
+                            result = num1 / num2;
+                        } else {
+                            Toast.makeText(MainActivity.this, "Cannot divide by zero", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
                         break;
+                    default:
+                        Toast.makeText(MainActivity.this, "Invalid operator", Toast.LENGTH_SHORT).show();
+                        return;
                 }
-                result.setText(resultInt + "");
+                resultTextview.setText(String.valueOf(result));
+
+
             }
         });
 
 
-    }
-
-    public static void hideKeyboard(Context context) {
-        View view = ((Activity) context).getCurrentFocus();
-        if (view != null) {
-            InputMethodManager imm = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
-            if (imm != null) {
-                imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
-            }
-        }
     }
 }
