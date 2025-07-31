1c2ddcf4de15eb634b74299db946913b52453c9f
ifahimkhan <khan.fahim95@hotmail.com>
2025-07-31 10:57:51 +0530

    committing code for layouts

--- app/src/main/java/com/fahim/mobileapplicationdevelopment_labwork/MainActivity.java
+++ app/src/main/java/com/fahim/mobileapplicationdevelopment_labwork/MainActivity.java
@@ -1,36 +1,17 @@
 package com.fahim.mobileapplicationdevelopment_labwork;
 
-import android.app.Activity;
-import android.content.Context;
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.view.View;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.ArrayList;
-
 public class MainActivity extends AppCompatActivity {
-
-    private EditText editTextUsername,editTextPassword;
-    private Button buttonLogin,buttonSignup;
-    private TextView textViewForgotPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.newfile);
-        editTextUsername = findViewById(R.id.editTextText);
-        editTextPassword = findViewById(R.id.editTextTextPassword);
-        buttonLogin = findViewById(R.id.button);
-        buttonSignup = findViewById(R.id.button2);
-        textViewForgotPassword = findViewById(R.id.textView2);
+        setContentView(R.layout.activity_main);
     }
 }
