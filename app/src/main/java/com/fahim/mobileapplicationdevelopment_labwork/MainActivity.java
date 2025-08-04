9af1a6379ea1ee4005c29ae54f0e645546136a1a
ifahimkhan <khan.fahim95@hotmail.com>
2025-08-04 16:23:20 +0530

    enable edge-to-edge display in MainActivity
    This commit enables edge-to-edge display in the `MainActivity` by calling `EdgeToEdge.enable(this)` and setting a `OnApplyWindowInsetsListener` to adjust padding for system bars. The root layout in `activity_main.xml` has also been given an ID for this purpose.

--- app/src/main/java/com/fahim/mobileapplicationdevelopment_labwork/MainActivity.java
+++ app/src/main/java/com/fahim/mobileapplicationdevelopment_labwork/MainActivity.java
@@ -3,7 +3,11 @@ package com.fahim.mobileapplicationdevelopment_labwork;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -22,7 +26,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
 
         ApiService apiService = RetrofitClient.getInstance().getApiService();
 
