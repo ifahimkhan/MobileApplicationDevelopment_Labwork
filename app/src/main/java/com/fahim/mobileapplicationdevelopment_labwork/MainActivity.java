2770099c003b653f4e4fed48be39d2b9edfcd5a5
ifahimkhan <khan.fahim95@hotmail.com>
2025-09-09 10:45:58 +0530

    Refactor: Implement image selection and capture functionality
    This commit replaces the RecyclerView for displaying images with functionality to:
    
    - Select an image from the gallery.
    - Capture an image using the camera.
    - Display the selected/captured image in an ImageView.
    
    The `ActivityResultLauncher` is now used for handling results from the gallery and camera intents, replacing the deprecated `onActivityResult` method. Glide is used for efficiently loading images into the ImageView.
    
    The permission handling for accessing media has been removed as the new implementation focuses on direct image selection/capture rather than browsing all media.

--- app/src/main/java/com/fahim/mobileapplicationdevelopment_labwork/MainActivity.java
+++ app/src/main/java/com/fahim/mobileapplicationdevelopment_labwork/MainActivity.java
@@ -1,64 +1,123 @@
 package com.fahim.mobileapplicationdevelopment_labwork;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
-import android.os.Build;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
 
-import androidx.annotation.NonNull;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
-    private static final int PERMISSION_REQUEST_CODE = 100;
-    private RecyclerView recyclerView;
+
+    private ImageView imageView;
+    /*
+    RequestOptions requestOptions = new RequestOptions()
+            .skipMemoryCache(true) // Focus on using Bitmap Pool
+            .diskCacheStrategy(DiskCacheStrategy.NONE);*/
+    ActivityResultLauncher<Intent> cameraLauncher;
+    ActivityResultLauncher<Intent> galleryLauncher;
+
+    public void selectImage(View view) {
+        Intent photoPickerIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        photoPickerIntent.setType("image/*");
+        galleryLauncher.launch(photoPickerIntent);
+//        startActivityForResult(Intent.createChooser(photoPickerIntent,"Pick Image"), 1);
+
+    }
+
+    public void captureImage(View view) {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        cameraLauncher.launch(Intent.createChooser(intent, "Camera"));
+//        startActivityForResult(intent,2);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageView = findViewById(R.id.imageView);
+        cameraLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult activityResult) {
+                        Intent data = activityResult.getData();
+                        Bundle extras = data.getExtras();
+                        Glide.with(imageView.getContext())
+                                .load(extras.get("data"))
+                                .into(imageView);/*
+                        Bitmap mBitmap = (Bitmap) extras.get("data");
+                        imageView.setImageBitmap(mBitmap);*/
+                    }
+                }
+        );
+        galleryLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult activityResult) {
+                        Intent data = activityResult.getData();
+                        Uri chosenImageUri = data.getData();
+                        Glide.with(imageView.getContext())
+                                .asBitmap()
+                                .load(chosenImageUri)
+                                .into(imageView);/*
 
-        recyclerView = findViewById(R.id.recyclerView);
-        recyclerView.setLayoutManager(new GridLayoutManager(this, 3));
+                        Bitmap mBitmap = null;
+                        try {
+                            mBitmap = MediaStore.Images.Media.getBitmap(MainActivity.this.getContentResolver(), chosenImageUri);
+                            imageView.setImageBitmap(mBitmap);
+                        } catch (IOException e) {
+                            Log.e("TAG", "error loading image", e);
+                            Toast.makeText(MainActivity.this, "error loading image", Toast.LENGTH_SHORT).show();
+                        }
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            // Android 13 and above: Request READ_MEDIA_IMAGES permission
-            if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_MEDIA_IMAGES)
-                    != PackageManager.PERMISSION_GRANTED) {
-                ActivityCompat.requestPermissions(this,
-                        new String[]{Manifest.permission.READ_MEDIA_IMAGES},
-                        PERMISSION_REQUEST_CODE);
-            } else {
-                loadImages();
+                    }
+                }
+        );*/
+                    }
+
+                });
+
+   /* @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == 1)
+        {
+            Uri chosenImageUri = data.getData();
+           *//* Glide.with(imageView.getContext())
+                    .asBitmap()
+                    .load(chosenImageUri)
+                    .apply(requestOptions)
+                    .into(imageView);*//*
+
+            Bitmap mBitmap = null;
+            try {
+                mBitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), chosenImageUri);
+                imageView.setImageBitmap(mBitmap);
+            } catch (IOException e) {
+                Log.e("TAG", "error loading image", e);
+                Toast.makeText(this, "error loading image", Toast.LENGTH_SHORT).show();
             }
-        } else  {
-            // Android 12 and below: Request READ_EXTERNAL_STORAGE permission
-            if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
-                    != PackageManager.PERMISSION_GRANTED) {
-                ActivityCompat.requestPermissions(this,
-                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
-                        PERMISSION_REQUEST_CODE);
-            } else {
-                loadImages();
-            }
+
         }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == PERMISSION_REQUEST_CODE) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                loadImages();
-            }
-        }
-    }
-
-    private void loadImages() {
-        ImageAdapter adapter = new ImageAdapter(this);
-        recyclerView.setAdapter(adapter);
+        else if (resultCode == RESULT_OK && requestCode == 2) {
+            Bundle extras = data.getExtras();
+            Bitmap mBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(mBitmap);
+           *//* Glide.with(imageView.getContext())
+                    .asBitmap()
+                    .load(extras.get("data"))
+                    .apply(requestOptions)
+                    .into(imageView);*/
     }
 }
